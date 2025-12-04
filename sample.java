4d381fd72aef7b29437c3b265b1ad2fbc354d359
meghanaSattarapu <meghanasattarapu2005@gmail.com>
2025-12-04 07:20:27 +0000

    commit

---
+++ sample.java
@@ -0,0 +1,11 @@
+Class Sample{
+    public static void main(String [] args){
+        int i = 1;
+        int f = 1;
+        int n = 5;
+        for(i=1;i<n;i++){
+            f=f*i;
+        }
+        System.out.println(f);
+    }
+}
